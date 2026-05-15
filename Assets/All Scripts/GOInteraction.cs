3df7f26bfcd0783cfd3a01cfceef210d1573f3d8
Bishu Maharjan <bishu1maharjan@gmail.com>
2026-05-15 15:20:24 +0545

    Ray casting was aligned infromation were added

--- Assets/All Scripts/GOInteraction.cs
+++ Assets/All Scripts/GOInteraction.cs
@@ -1,18 +1,24 @@
 using UnityEngine;
+
 public class GOInteraction : MonoBehaviour
 {
-private bool interaction = false;
-public bool Interaction
-{
-get { return interaction; }
-set { interaction = value; }
-}
-// Start is called before the first frame update
-void Start()
-{
-}
-// Update is called once per frame
-void Update()
-{
-}
+    [SerializeField]
+    private GameObject m_InteriorObject;
+
+    private bool m_IsVisible = false;
+
+    void Start()
+    {
+        m_InteriorObject.SetActive(false);
+    }
+
+    public bool Interaction
+    {
+        get => m_IsVisible;
+        set
+        {
+            m_IsVisible = !m_IsVisible;
+            m_InteriorObject.SetActive(m_IsVisible);
+        }
+    }
 }
